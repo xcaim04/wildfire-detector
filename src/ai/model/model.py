41eaf9820759620ef37fe52b34562fc4069ff0ff
Carlos Blanco <carlojbm04@gmail.com>
2026-06-15 11:36:14 -0400

    Build a Neural Network with Pytorch

---
+++ src/ai/model/model.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+
+class WildfireNet(nn.Module):
+    
+    def __init__(self, input_dim=11):
+        super(WildfireNet, self).__init__()
+
+        self.network = nn.Sequential(
+            nn.Linear(input_dim, 32),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            
+            nn.Linear(32, 16),
+            nn.ReLU(),
+            nn.Dropout(p=0.2),
+            
+            nn.Linear(16, 8),
+            nn.ReLU(),
+            
+            nn.Linear(8, 1),
+            nn.Sigmoid()
+        )
+        
+    def forward(self, x):
+        return self.network(x)
